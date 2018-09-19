15c0e0f80c86d7bb2438e1f3ef593db35d768ad0
Guillermo Brinatti <guillermobrinatti@gmail.com>
2018-09-19 19:31:01 -0300

    Funciones de calibracion y caracterizacion del ruido

--- PlacaAudio.py
+++ PlacaAudio.py
@@ -385,7 +385,17 @@ def play_rec(
     elif output_n_channels == 2:
         return demultiplex(ReadBuffer)
 
+def volt2output(value):
+    return (value)/1.62
 
+def output2volt(value):
+    return 1.62*value
+
+def input2volt(value):
+    return 2.555*value
+
+def volt2input(value):
+    return (value)/2.555
 
 
 if __name__ == "__main__":
@@ -394,52 +404,32 @@ if __name__ == "__main__":
     
     sample_freq = 192000
     
-    signal_amp  = 2
-    signal_freq = 1000
+    signal_amp  = 1
+    signal_freq = 10000
     signal_len  = sample_freq * 2
     total_time = 1
     #channel_right  = np.array(signal_amp * np.sin(2*np.pi*signal_freq * np.arange(signal_len)/sample_freq)).astype(np.float32)
-    channel_left = function_generator("sine", signal_freq, sample_freq, signal_amp,signal_len,np.float32)
+    channel_left = function_generator("sine", signal_freq, sample_freq, signal_amp,signal_len, np.float32)
     #channel_right = np.zeros((signal_len,), np.float32)
     channel_right = channel_left
-    (out_left, out_right) = play_rec(in_data = (channel_left, channel_right),
+    (signal_left, signal_right) = play_rec(in_data = (channel_left, channel_right),
                                      sample_freq = sample_freq,
                                      read_time_limit = total_time,
                                      write_time_limit = total_time,
-                                     )
-    # me quedo con el centro del vector
-    Imin = len(out_left)//2
-    Imax = 9*len(out_left)//10
-    L = out_left[Imin:Imax]
-    R = out_right[Imin:Imax]
-    
-    calibracion = 1/.414    
-    resistencia = 1000
-    I = L*calibracion/resistencia * 1000 #corriente en mA
-    V = (R-L)*calibracion
+                                        )
+    eje_tiempo = np.arange(0,len(signal_left))/sample_freq
     
     plt.figure()
-    plt.plot(L*calibracion, label = "L")
-    plt.plot(R*calibracion, label = "R")
-    plt.legend()
+    plt.plot(eje_tiempo,input2volt(signal_left))
+    plt.xlabel("Tiempo [seg]")
+    plt.ylabel("Tensión [V]")
+    plt.title("Ruido canal izquierdo")
     
     plt.figure()
-    plt.plot(V,I)
-    
-    sort_idx = np.argsort(V)
-    V = V[sort_idx]
-    I = I[sort_idx]
-    
-    I_smooth = scipy.signal.savgol_filter(I, 2001, 2)
-    
-    Vmean = np.mean(L[np.abs(R)<0.025])*calibracion
-    
-    plt.figure()
-    #plt.plot(V,I)
-    #plt.figure()
-    plt.plot(V + Vmean, I_smooth-Vmean/resistencia*1000)
-    plt.grid()
-    
-    
-    
+    plt.plot(eje_tiempo,input2volt(signal_right))    
+    plt.xlabel("Tiempo [seg]")
+    plt.ylabel("Tensión [V]")
+    plt.title("Ruido canal derecho")
 
+    L = len(signal_left)
+    noise = np.std(signal_left[(L//5):-1])
